e7647395e118c44e434e467fdae06a7fcd70a823
Ted Neward <ted@tedneward.com>
2025-10-11 13:19:35 -0700

    Bugs fixed

--- linguis/tests/test_fns.py
+++ linguis/tests/test_fns.py
@@ -21,23 +21,29 @@ def test_functiondecl() -> None:
                         ["a", "b"], 
                         Block(
                             [Return(BinaryOp(Identifier("a"), "+", Identifier("b")))]))
+    func.eval(env)
 
     # x = 2;
     # y = 3;
     assign_x = Assignment("x", Number(2))
+    assign_x.eval(env)
     assign_y = Assignment("y", Number(3))
+    assign_y.eval(env)
 
     # z = add(x, y);
     call_add = Assignment("z", FunctionCall(Identifier("add"), [Identifier("x"), Identifier("y")]))
+    call_add.eval(env)
 
     # println("Result:", z)
     print_call = FunctionCall(Identifier("println"), [String("Result:"), Identifier("z")])
+    print_call.eval(env)
 
     program = Block([func, assign_x, assign_y, call_add, print_call])
-
-    # Execute
-    program.eval(env)
 
     # Verify
     assert env.get("z") == 5
     assert env.get("add") is not None
+
+    # Execute
+    program.eval(env)
+
